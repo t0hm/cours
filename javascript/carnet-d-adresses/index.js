f287c953350f1c09c8484c34056c30e050a37305
Thomas COLOMBET <contact@thomascolombet.fr>
2023-04-22 21:47:23 +0200

    index.js

---
+++ javascript/carnet-d-adresses/index.js
@@ -0,0 +1,22 @@
+let carnetAdresses = {};
+
+carnetAdresses["Dupont"] = {
+  nom: "Dupont",
+  prenom: "Jean",
+  telephone: "01 23 45 67 89",
+  email: "jean.dupont@email.com"
+};
+
+function ajouterContact(contact) {
+}
+
+function trouverContact(nom) {
+
+}
+
+function supprimerContact(nom) {
+}
+
+function afficherContacts() {
+
+}
